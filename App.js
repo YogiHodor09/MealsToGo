920a7cbbbe2ab11c146274a7d547af39562ba676
TECHMAHINDRA\YP00818780 <yp00818780@techmahindra.com>
2022-03-25 17:52:34 +0530

    added styles and listview for restaurant card..

--- App.js
+++ App.js
@@ -1,38 +1,12 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import { RestaurantsScreen } from "./src/features/restaurants/restautants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.safeAreaContainer}>
-        <View style={styles.search}>
-          <Text>Search</Text>
-        </View>
-
-        <View style={styles.list}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  safeAreaContainer: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-
-  search: {
-    padding: 16,
-    backgroundColor: "green",
-  },
-
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
